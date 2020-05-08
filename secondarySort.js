cf090fefb9583d54e8073292729c3b183238dbba
jjhiggz <jonathan.higger@gmail.com>
2020-05-07 19:39:54 -0600

    cleaned secondarySort

--- secondarySort.js
+++ secondarySort.js
@@ -1,9 +1,12 @@
-// this function does what you need it to
-sortByXthenY = ( arrayOfObjects, keyX, keyY ) => {
-  let sort1 = arrayOfObjects.sort((a,b) => a[keyX]-b[keyX])
+// This is my attempt at a secondary sort. I was helping a friend with a project, and this was the solution I helped him come up with
 
-  let groupings = []
+// In his circumstance he had to return scores for a scoreboard on a trivia app he made. He wanted the return to primarily sort by number of incorrect answers (from least to greatest) then to sort secondarily based off of the time that it took to complete the trivia. Thiswas the result I came up with 
+
+sortByKeyXThenByKeyY = ( arrayOfObjects, keyX, keyY ) => {
+  const sort1 = arrayOfObjects.sort((a,b) => a[keyX]-b[keyX])
+
   let distinctValues = []
+
   sort1.forEach(object => {
     if( !(distinctValues.includes(object[keyX])) ){
       distinctValues = [...distinctValues, object[keyX]]
@@ -15,12 +18,12 @@ sortByXthenY = ( arrayOfObjects, keyX, keyY ) => {
   })
   
   arrayOfSortedGroupedObjects = arrayOfGroupedObjects.map(arrayOfObjects => {
-    return arrayOfObjects.sort((a,b) => a[keyY] - b[keyY])
+    return arrayOfObjects.sort( (a,b) => a[keyY] - b[keyY] )
   })
   return arrayOfSortedGroupedObjects.flat()
 }
 
-//this proves it 
+//this is just some test data
 const arrayOfObjects = [
   {
       totalMisses: 0,
@@ -48,4 +51,4 @@ const arrayOfObjects = [
   },
 
 ]
-console.log(sortByXthenY(arrayOfObjects,'totalMisses','secondsToComplete'))
+console.log(sortByKeyXThenByKeyY(arrayOfObjects,'totalMisses','secondsToComplete'))
